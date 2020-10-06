3e7ea827d4920b0ffafeb7ff79a707530215a115
jstloyal <jstloyalty@gmail.com>
2020-10-06 07:07:50 +0100

    Improve the game background and add alien and soldier image

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import Phaser from 'phaser';
 import './styles/style.css';
 import config from './config/config';
+import GameScene from './scenes/GameScene';
+
+class Game extends Phaser.Game {
+  constructor() {
+    super(config);
+    this.scene.add("Game", GameScene);
+  }
+}
+
+window.game = new Game();
